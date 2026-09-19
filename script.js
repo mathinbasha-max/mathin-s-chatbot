d62f8f2506f888317a18a53b65ba2bf6468d233d
mathinbasha-max <mathinmathu37@gmail.com>
2026-09-20 01:42:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function sendMessage() {
 
     // Send to Python
     try {
-        const response = await fetch("http://127.0.0.1:5000/chat", {
+        const response = await fetch("http:/chat", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
