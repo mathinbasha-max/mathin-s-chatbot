694b94067116ee5bd1f11f9c036d14939219e937
mathinbasha-max <mathinmathu37@gmail.com>
2026-09-20 01:29:05 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,6 +6,9 @@ import os
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def home():
+    return "My AI Chatbot Server is Running!"
 @app.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json()
